7ec7c7c4470da812943061cce55b6d4248a173e0
Pedro Ivo <pedroooivo@hotmail.com>
2023-04-06 19:06:03 -0300

    Jogo da Velha

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,44 +12,44 @@ function checkForWin(currentPlayer) {
     /*Horizontal*/
     if(celulas[0].innerText === "X" && celulas[1].innerText === "X" && celulas[2].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)   
     }
     if(celulas[3].innerText === "X" && celulas[4].innerText === "X" && celulas[5].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)     
     }
     if(celulas[6].innerText === "X" && celulas[7].innerText === "X" && celulas[8].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)   
     }
     /*Vertical*/
     if(celulas[0].innerText === "X" && celulas[3].innerText === "X" && celulas[6].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)    
     }
     if(celulas[1].innerText === "X" && celulas[4].innerText === "X" && celulas[7].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)     
     }
     if(celulas[2].innerText === "X" && celulas[5].innerText === "X" && celulas[8].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)     
     }
     /*Diagonal*/
     if(celulas[0].innerText === "X" && celulas[4].innerText === "X" && celulas[8].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10)    
     }
     if(celulas[2].innerText === "X" && celulas[4].innerText === "X" && celulas[6].innerText === "X"){
         setTimeout(function() {
-            alert("X ganhou!");
+            window.alert("X ganhou!");
           }, 10) 
     }
                                         /*WIN PARA O*/
@@ -57,44 +57,44 @@ function checkForWin(currentPlayer) {
      /*Horizontal*/
      if(celulas[0].innerText === "O" && celulas[1].innerText === "O" && celulas[2].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)     
     }
     if(celulas[3].innerText === "O" && celulas[4].innerText === "O" && celulas[5].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)    
     }
      if(celulas[6].innerText === "O" && celulas[7].innerText === "O" && celulas[8].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)     
     }
      /*Vertical*/
      if(celulas[0].innerText === "O" && celulas[3].innerText === "O" && celulas[6].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)     
      }
     if(celulas[1].innerText === "O" && celulas[4].innerText === "O" && celulas[7].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)   
     }
      if(celulas[2].innerText === "O" && celulas[5].innerText === "O" && celulas[8].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)    
     }
     /*Diagonal*/
     if(celulas[0].innerText === "O" && celulas[4].innerText === "O" && celulas[8].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)     
     }
      if(celulas[2].innerText === "O" && celulas[4].innerText === "O" && celulas[6].innerText === "O"){
         setTimeout(function() {
-            alert("O ganhou!");
+            window.alert("O ganhou!");
           }, 10)   
     }
 }
